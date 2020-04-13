6fc14a0c3d4032054dfd475228575671134d4a63
robertburrowes <r.burrowes@auckland.ac.nz>
2020-04-13 22:55:45 +1200

    Add __dir__ path to access testfile

--- test/test_wikk_aes_256.rb
+++ test/test_wikk_aes_256.rb
@@ -9,14 +9,14 @@ end
 
 def test_instance_lvl_encryption
   aes2 = WIKK::AES_256.new
-  File.open("testfile.txt",'r') do |fd|
+  File.open(__dir__ + "/testfile.txt",'r') do |fd|
     @et = aes2.cipher_to_s(fd)
   end
   puts aes2.decrypt(@et, true)
 end
 
 def test_class_lvl_encryption
-  File.open("testfile.txt",'r') do |fd|
+  File.open(__dir__ + "/testfile.txt",'r') do |fd|
     @et2, @key, @iv = WIKK::AES_256.cipher_to_s(fd)
   end
   puts WIKK::AES_256.decrypt(@et2, true, @key, @iv)
